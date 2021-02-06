1ad5c72b7ab95c26d76506bb0d5563efc625fd14
Kingsley Enoka <kuenoka@mix.wvu.edu>
2021-02-06 13:50:46 -0600

    first commit

---
+++ SQLProject/DBHelper.swift
@@ -0,0 +1,8 @@
+//
+//  DBHelper.swift
+//  SQLProject
+//
+//  Created by Kingsley Enoka on 1/5/21.
+//
+
+import Foundation
---
+++ SQLProject/Person.swift
@@ -0,0 +1,8 @@
+//
+//  Person.swift
+//  SQLProject
+//
+//  Created by Kingsley Enoka on 1/5/21.
+//
+
+import Foundation
